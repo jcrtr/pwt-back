128c21e43cdd73f20c3d2f549387559a95c0792c
likiblack <likiblack@gmail.com>
2024-03-17 20:58:31 +0300

    first

--- pwt/settings.py
+++ pwt/settings.py
@@ -69,7 +69,9 @@ REST_FRAMEWORK = {
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'http://31.172.67.18:3000',
+    'http://31.172.67.18'
 ]
 CSRF_TRUSTED_ORIGINS = ['http://localhost:3000', 'http://127.0.0.1:3000']
 
